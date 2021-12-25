c05683f349b1e1e32b2ddfc339966770fba1657e
Prajwal Horti <hortiprajwal@gmail.com>
2021-12-25 09:32:48 +0530

    add files

---
+++ lib/model/article_model.dart
@@ -0,0 +1,39 @@
+// create the article model
+
+import 'source_model.dart';
+
+class Article {
+  Source source;
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+
+  //constructor
+  Article(
+      {this.source,
+      this.author,
+      this.title,
+      this.description,
+      this.url,
+      this.urlToImage,
+      this.publishedAt,
+      this.content});
+
+  //create function that will map the json into a list
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      source: Source.fromJson(json['source']),
+      author: json['author'] as String,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      url: json['url'] as String,
+      urlToImage: json['urlToImage'] as String,
+      publishedAt: json['publishedAt'] as String,
+      content: json['content'] as String,
+    );
+  }
+}
---
+++ lib/model/source_model.dart
@@ -0,0 +1,14 @@
+//source model class
+
+class Source {
+  String id;
+  String name;
+
+  //Let's create the constructor
+  Source({this.id, this.name});
+
+  //Let's create the factory function to map the json
+  factory Source.fromJson(Map<String, dynamic> json) {
+    return Source(id: json['id'], name: json['name']);
+  }
+}
